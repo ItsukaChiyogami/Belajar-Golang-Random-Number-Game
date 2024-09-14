ca6c09a99fbb3806f4492b2de0cc38d411454ab6
ItsukaChiyogami <origamikerja@gmail.com>
2024-09-14 18:36:59 +0800

    update game randomnumber

--- randomnumber/randomgame.go
+++ randomnumber/randomgame.go
@@ -6,6 +6,9 @@ import (
 )
 
 func main() {
+
+	heal := 10
+
 	min := 1
 	max := 30
 	random := rand.Intn(max-min+1) + min
@@ -13,16 +16,25 @@ func main() {
 	var user int
 
 	fmt.Println("tembak angka sampai 1-30")
-	fmt.Scan(&user)
+	for heal > 0 {
+		fmt.Printf("Anda memiliki %d heal tersisa\n", heal)
+		fmt.Print("Masukkan tebakan: ")
+		fmt.Scan(&user)
 
-	if user < min || user > max {
-		fmt.Println("anda salah imput,harap input 1-30")
-	} else if user == random {
-		fmt.Println("anda benar")
-	} else if user < random {
-		fmt.Println("nomor terlalu kecil")
-	} else {
-		fmt.Println("Nomor Terlalu Besar")
+		if user < min || user > max {
+			heal--
+			fmt.Println("anda salah imput,harap input 1-30")
+		} else if user == random {
+			fmt.Println("anda benar")
+		} else if user < random {
+			heal--
+			fmt.Println("nomor terlalu kecil")
+		} else {
+			heal--
+			fmt.Println("Nomor Terlalu Besar")
+		}
+		if heal == 0 {
+			fmt.Println("game over")
+		}
 	}
-
 }
